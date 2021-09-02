0c36bf5997947bd4f3a94485c193c5d7fa4c18ac
dprymudrau <dprymudrau@solvd.com>
2021-09-02 18:12:58 +0300

    write to file implemented

--- src/main/java/com/solvd/course/jackson/app/EntryPoint.java
+++ src/main/java/com/solvd/course/jackson/app/EntryPoint.java
@@ -1,7 +1,6 @@
 package com.solvd.course.jackson.app;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solvd.course.jackson.binary.User;
@@ -20,12 +19,23 @@ public class EntryPoint {
         try {
             JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, User.class);
 //            TypeReference<List<User>> type = new TypeReference<List<User>>() {}; - can be used as an option
+            log.info("reading users from file");
             List<User> users = objectMapper.readValue(new File("src/main/resources/jackson/example.json"), type);
             log.info(users.toString());
+            log.info("writing users to file");
+            writeToJson(users, objectMapper);
         } catch (JsonProcessingException e) {
             log.error("Error while parsing json file", e);
         } catch (IOException e) {
             log.error("Error while reading file", e);
         }
+    }
+
+    private static void writeToJson(List<User> users, ObjectMapper objectMapper) throws IOException {
+        File result = new File("src/main/resources/jackson/resultExample.json");
+        if(!result.exists()) {
+            result.createNewFile();
+        }
+        objectMapper.writeValue(result, users);
     }
 }
